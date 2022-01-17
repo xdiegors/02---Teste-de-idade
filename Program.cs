4cd0457d680b1d57a65625e687b5e19901a9f038
xdiegors <xdiegors@outlook.com>
2022-01-17 19:38:57 -0300

    Teste de if else C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Me diga seu nome e sua idade!");
+            string nome = Console.ReadLine();
+            int idade = int.Parse(Console.ReadLine());
+            if (idade < 18)
+            {
+                Console.WriteLine(nome + ", você não pode beber!!!");
+            } else
+            {
+                Console.WriteLine("Parabéns " + nome + ", pode encher a cara!!!");
+            }
+        }
+    }
+}
